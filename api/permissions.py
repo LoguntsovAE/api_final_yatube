6a6d3ffe615dcc582c9984a11a5fdb6a6099b5ec
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-04 22:24:43 +0500

    Исправлены замечания 2 ревью.4

--- api/permissions.py
+++ api/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS or
-                    obj.author == request.user)
+                obj.author == request.user)
