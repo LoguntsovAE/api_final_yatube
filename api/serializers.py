58494552b60e81e68007a452378dba0aac555f52
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-04 22:18:15 +0500

    Исправлены замечания 2 ревью.4

--- api/serializers.py
+++ api/serializers.py
@@ -53,10 +53,8 @@ class FollowSerializer(serializers.ModelSerializer):
         ]
 
     def validate(self, data):
-        if (
-            self.context['request'].method == 'POST' and
-            data['user'] == data['following']
-        ):
+        if (self.context['request'].method == 'POST' and
+                data['user'] == data['following']):
             raise serializers.ValidationError(
                 'Вы не можете сами на себя подписаться!'
             )
